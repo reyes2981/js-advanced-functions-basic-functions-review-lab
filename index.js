0e2779cb7508f5552debb8761f00fcd26ab047c7
noe <noe.reyes92@yahoo.com>
2021-08-05 09:03:28 -0500

    commit tree b7c3c4d1ba14913bdbf0591b9014808a2ae01a14

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+}
+
+const Calculator = {
+    add: function(a,b) {
+        return a + b
+    },
+    subtract: function(a,b) {
+        return a - b
+    },
+    multiply: function(a,b) {
+        return a * b
+    },
+    divide: function(a,b) {
+        return a / b
+    },
+}
+
+function actionApplyer(start, arr) {
+    let a = start
+
+    for (let i = 0; i < arr.length; i++ ){
+      a = arr[i](a)
+    }
+  
+    return a
+}
